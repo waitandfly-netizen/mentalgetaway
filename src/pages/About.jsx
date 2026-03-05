ca834043a89051dd831301f47c447adf6e449b4d
朱雲 <waitandfly@gmail.com>
2026-03-05 14:52:00 +0000

    File changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -258,7 +258,7 @@ export default function About() {
               },
               {
                 year: "2023", tag: "重新出發",
-                img: "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/698fc983574e659f561934f1/0c55c486a_2023.jpg",
+                img: "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/698fc983574e659f561934f1/9475c6f06_2023.jpg",
                 content: "疫情過後重新出發，在宜蘭與南投的好山好水中，撫平疫情帶來的負擔。",
                 detail: "5月宜蘭外澳、9月暨南大學，共計2場次。"
               },
