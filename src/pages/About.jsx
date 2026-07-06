e51aed0c8038f2dfb02d0e5f68fe32fc3f52c32a
朱雲 <waitandfly@gmail.com>
2026-07-06 12:53:04 +0000

    File changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -235,7 +235,7 @@ export default function About() {
               },
               {
                 year: "2016", tag: "走向更廣的自然",
-                img: null,
+                img: "https://media.base44.com/images/public/698fc983574e659f561934f1/8ad866b99_2016.jpg",
                 content: "第一次離開埔里，來到宜蘭外澳海邊，將大海獨有的鮮活能量分享給參加者。",
                 detail: "4月宜蘭外澳、7月暨大、10月埔里松濤園，共計3場次。"
               },
