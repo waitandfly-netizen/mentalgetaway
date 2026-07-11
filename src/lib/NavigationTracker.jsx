ebc7ca1fbf8234b3380e14d1347578555f5533af
朱雲 <waitandfly@gmail.com>
2026-07-11 13:17:42 +0000

    File changes

--- src/lib/NavigationTracker.jsx
+++ src/lib/NavigationTracker.jsx
@@ -10,6 +10,11 @@ export default function NavigationTracker() {
     const { Pages, mainPage } = pagesConfig;
     const mainPageKey = mainPage ?? Object.keys(Pages)[0];
 
+    // Scroll to top on every page navigation
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location.pathname]);
+
     // Log user activity when navigating to a page
     useEffect(() => {
         // Extract page name from pathname
